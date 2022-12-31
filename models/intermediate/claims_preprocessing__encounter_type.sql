2943f3528a858b0eae455aa738c00413e5032f21
Mtully@iconhealthco.com <Mtully@iconhealthco.com>
2022-12-31 18:52:36 +0000

    runnable

--- models/intermediate/claims_preprocessing__encounter_type.sql
+++ models/intermediate/claims_preprocessing__encounter_type.sql
@@ -8,17 +8,18 @@
 with unmapped as (
     select distinct claim_id
     from {{ ref('claims_preprocessing__medical_claim') }}
-    where claim_id not in (select claim_id from {{ref ('intermediate_encounter_type_temp') }} )
+    --where claim_id not in (select claim_id from `ferrous-weaver-306014`.`LDS_tuva_core`.`intermediate_encounter_type_temp` )
+    where claim_id not in (select claim_id from {{ ref('claims_preprocessing__encounter_type_temp') }} )
 ),
-
 
 encounter_types as (
     select
     claim_id,
     encounter_type,
     encounter_type_detail
-    from {{ref ('intermediate_encounter_type_temp') }} )
-
+    --from `ferrous-weaver-306014`.`LDS_tuva_core`.`intermediate_encounter_type_temp` )
+    from {{ ref('claims_preprocessing__encounter_type_temp') }}
+    
     union all
 
     select
